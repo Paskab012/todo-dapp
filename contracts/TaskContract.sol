c8069f82f3816bdb0b607e5d75345316670d0c22
paskab012 <pascalkabikampan@gmail.com>
2023-02-24 17:57:54 +0200

    Backend project completed

--- contracts/TaskContract.sol
+++ contracts/TaskContract.sol
@@ -17,16 +17,16 @@ contract TaskContract {
     mapping(uint256 => address) taskToOwner;
 
     function addTask(string memory taskText, bool isDeleted) external {
-        uint taskId = tasks.lenght;
+        uint taskId = tasks.length;
         tasks.push(Task(taskId, taskText, isDeleted));
         taskToOwner[taskId] = msg.sender;
         emit AddTask(msg.sender, taskId);
     }
 
     function getMyTasks() external view returns (Task[] memory) {
-        Task[] memory temporary = new Task[](tasks.lenght);
+        Task[] memory temporary = new Task[](tasks.length);
         uint counter = 0;
-        for(uint i=0; i<tasks.lenght; i++){
+        for(uint i=0; i<tasks.length; i++){
             if(taskToOwner[i] == msg.sender && tasks[i].isDeleted == false) {
                 temporary[counter] = tasks[i];
                 counter++;
